74419c9353707b7f858bbb400872d72427f1281c
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2021-08-24 11:17:59 +0100

    Update omni_RMX2020.mk

--- omni_RMX2020.mk
+++ omni_RMX2020.mk
@@ -20,25 +20,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 LOCAL_PATH := device/realme/RMX2020
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
-
-# Inherit from our custom product configuration
-#$(call inherit-product, vendor/omni/config/common.mk)
-
-## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2020
 PRODUCT_NAME := omni_RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme C3/Narzo 10A
 PRODUCT_MANUFACTURER := realme
-
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
